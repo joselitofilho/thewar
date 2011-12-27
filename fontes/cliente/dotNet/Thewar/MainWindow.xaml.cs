87704385eef02dc399d213f67a0fa5d9f4edf379
unknown <IEPRO@Neyrobson.(none)>
2011-12-27 15:01:58 -0300

    Versão base do cliente em .NET 4.0.

---
+++ fontes/cliente/dotNet/Thewar/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using br.com.thewar;
+using Newtonsoft.Json;
+using br.com.thewar.protocolo;
+using Newtonsoft.Json.Linq;
+using br.com.thewar.protocol;
+
+namespace Thewar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            thiss = this;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="json"></param>
+        public static void ProcessResponse(string json)
+        {
+            JObject jObj = JObject.Parse(json);
+            JToken jTk;
+
+            // Descobrindo o tipo da resposta
+            string tipo = jObj.Property("type").Value.ToString();
+
+            // Pegando o campo data com os atributos do objeto.
+            jTk = jObj.Property("data").Value;
+
+            JsonSerializer serializer = new JsonSerializer();
+
+            // Login
+            if (tipo.Equals("loginresponse"))
+            {
+                LoginResponse l = (LoginResponse)serializer.Deserialize(new JTokenReader(jTk), typeof(LoginResponse));
+                if (l.Status == 0)
+                {
+                    MessageBox.Show("Login com sucesso...");
+
+                    // TODO: Redirecionar página.
+                    //thiss.Child
+                }
+                else
+                {
+                    thiss.LoginView.processResponse(l);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Interface de comunicação com o servidor. Ela será utilizada em toda a aplicação.
+        /// </summary>
+        public static CommunicationInterface communication = new CommunicationInterface("127.0.0.1", 1234);
+        public static MainWindow thiss;
+        //public static InterfaceComunicacao comunicacao = new InterfaceComunicacao("192.168.1.9", 1234);
+    }
+}
---
+++ fontes/cliente/dotNet/Thewar/br/com/thewar/LoginTela.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using br.com.thewar.protocolo;
+using br.com.thewar.protocol;
+
+namespace Thewar.br.com.thewar
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class LoginTela : UserControl
+    {
+        #region Delegates
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginRequest"></param>
+        public delegate void processResponseDelegate(LoginResponse loginRequest);
+        #endregion
+
+        public LoginTela()
+        {
+            InitializeComponent();
+        }
+
+        public void processResponse(LoginResponse loginRequest)
+        {
+            if (loginRequest.Status != 0)
+            {
+                MessageBox.Show("Error codi: " + loginRequest.Status, "Login", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                // É isso mesmo! Para atualizar o botão precisa de toda essa novela!
+                btLogin.Dispatcher.Invoke(
+                    System.Windows.Threading.DispatcherPriority.Normal,
+                    new Action(
+                      delegate()
+                      {
+                          btLogin.IsEnabled = true;
+                      }
+                  ));
+            }
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Desabilita o botão de login.
+                btLogin.IsEnabled = false;
+
+                // Cria a requisição de login
+                LoginRequest login = new LoginRequest()
+                {
+                    Nick = tbEmail.Text,
+                    Pass = tbSenha.Text
+                };
+
+                // Envia a requisição.
+                MainWindow.communication.SendObject(login);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
