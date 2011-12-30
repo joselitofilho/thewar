af7f6501c97f543889ab825aa9764c0f6bfbbced
unknown <IEPRO@Neyrobson.(none)>
2011-12-30 10:24:54 -0300

    Classe da resposta referente ao item do protocolo UserLogged.

---
+++ fontes/servidor/java/server/src/main/java/br/com/thewar/server/response/UserLoggedResponse.java
@@ -0,0 +1,30 @@
+package br.com.thewar.server.response;
+
+/**
+ * Class that encapsulate the login response
+ * 
+ * @author Joselito Viveiros - joselitofilhoo@gmail.com
+ * 
+ */
+public class UserLoggedResponse extends AbstractResponse {
+
+	/**
+	 * Create the response of user logged protocol
+	 */
+	public UserLoggedResponse() {
+
+		super(UserLoggedResponse.class.getSimpleName().toLowerCase());
+
+	}
+	
+	/**
+	 * Set the nick of response
+	 * 
+	 * @param nick of response
+	 */
+	public void setStatus(String nick) {
+	
+		data.put("nick", nick);
+
+	}
+}
