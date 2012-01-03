8d369f639e94cbe7b4b7967347ac100802e12c0e
Joselito Viveiros <joselitofilhoo@gmail.com>
2012-01-03 00:00:44 -0300

    Adicionado requisição RoomChangeRequest.

---
+++ fontes/cliente/dotNet/Thewar/br/com/thewar/protocol/request/RoomChangeRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace br.com.thewar.protocol.request
+{
+    public class RoomChangeRequest
+    {
+        #region Propriedades
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Pos { get; set; }
+        #endregion
+    }
+}
