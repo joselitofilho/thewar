c41d41d3249f35aa10205abb171f563a46c2ddcd
Joselito Viveiros <joselitofilhoo@gmail.com>
2012-01-03 00:02:04 -0300

    Implementação inicial da requisição de uma sala.

--- fontes/cliente/dotNet/Thewar/br/com/thewar/view/RoomItensControl.xaml.cs
+++ fontes/cliente/dotNet/Thewar/br/com/thewar/view/RoomItensControl.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using br.com.thewar.protocol.request;
+using br.com.thewar.lang;
+using br.com.thewar;
 
 namespace Thewar.br.com.thewar.view
 {
@@ -26,7 +29,34 @@ namespace Thewar.br.com.thewar.view
 
         private void TextBlock_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("click");
+            // Instância da sessão.
+            Session session = Session.getSession();
+
+            // É verificado se existe alguma requisição (RoomChangeRequest) na sessão pendente.
+            // Caso não exista, a requisição é enviada ao servidor e colocada na sessão para
+            // posteriormente ser processada, quando o servidor responder.
+            if (!session.ContainsKey("RoomChangeReq"))
+            {
+                // Extrai as informações do x:Name do TextBlock na forma rX_pY.
+                // r: Id da sala            X: Valor correspondente
+                // p: Posição na sala       Y: Valor correspondente
+                string roomPositionStr = ((TextBlock)sender).Name; /*rX_pY*/
+                int roomId = Convert.ToInt32(roomPositionStr.Substring(1, roomPositionStr.LastIndexOf('_') - 1));
+                int position = Convert.ToInt32(roomPositionStr.Substring(roomPositionStr.LastIndexOf('_') + 2));
+
+                // Cria a requisição para o servidor.
+                RoomChangeRequest roomChangeReq = new RoomChangeRequest()
+                {
+                    Id = roomId,
+                    Pos = position
+                };
+
+                // Atualiza a requisição na sessão.
+                session["RoomChangeReq"] = roomChangeReq;
+
+                // Send the request to the server.
+                MainManager.Communication.SendObject(roomChangeReq);
+            }
         }
     }
 }
