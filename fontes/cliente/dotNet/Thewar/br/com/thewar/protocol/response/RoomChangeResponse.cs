e5dcdad18019c59665ae7247cf514ee8a7326087
Joselito Viveiros <joselitofilhoo@gmail.com>
2012-01-04 10:17:56 -0300

    Implementado item Room Change do protocolo.

---
+++ fontes/cliente/dotNet/Thewar/br/com/thewar/protocol/response/RoomChangeResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace br.com.thewar.protocol.response
+{
+    public class RoomChangeResponse
+    {
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Room { get; set; }
+        public string Pos { get; set; }
+        public string Status { get; set; }
+        #endregion
+    }
+}
