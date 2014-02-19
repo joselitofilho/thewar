f508ee891bfab397a17546c11f852dbb7bf59605
Joselito Viveiros <joselitofilhoo@gmail.com>
2014-02-19 23:18:26 +0000

    Teste para metodo de atualizar pontuacao na base.

--- implementacao/server/test/PontuacaoDBTeste.py
+++ implementacao/server/test/PontuacaoDBTeste.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import shutil
 import unittest
 from src.pontuacaodb import *
 
 class PontuacaoDBTeste(unittest.TestCase):
-    
+   
+    def setUp(self):
+        try:
+            os.remove('wartest.db')
+        except:
+            pass
+
+        shutil.copyfile('wartest_base.db', 'wartest.db')
+
     def test_DeveRetornarAPontuacaoDBODoUsuario(self):
         # Preparação.
         pontuacaoDBOEsperado = PontuacaoDBO()
@@ -17,9 +27,28 @@ class PontuacaoDBTeste(unittest.TestCase):
         # Operação.
         pontuacaoDB = PontuacaoDB('wartest.db')
         pontuacaoDBO = pontuacaoDB.pontuacaoDBODoUsuario("Joselito")
-        
+
         # Verificação.
         self.assertEqual(pontuacaoDBOEsperado, pontuacaoDBO)
+
+    def test_DeveAtualizarPontuacaoDBODoUsuario(self):
+        # Preparação.
+        novaPontuacaoDBO = PontuacaoDBO()
+        novaPontuacaoDBO.pontos = 9999;
+        novaPontuacaoDBO.quantidadeDePartidas = 99
+        novaPontuacaoDBO.quantidadeDeVitorias = 111
+        novaPontuacaoDBO.quantidadeDestruido = 0
+
+        # Operação.
+        pontuacaoDB = PontuacaoDB('wartest.db')
+        pontuacaoDBO = pontuacaoDB.atualizaPontuacaoDBOParaUsuario("Joselito", novaPontuacaoDBO)
+        pontuacaoDBOAtualizada = pontuacaoDB.pontuacaoDBODoUsuario("Joselito")
+
+        # Verificação.
+        self.assertEqual(novaPontuacaoDBO, pontuacaoDBOAtualizada)
+
+    def tearDown(self):
+        os.remove('wartest.db')
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
