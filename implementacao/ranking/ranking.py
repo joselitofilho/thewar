6de436f8fd8d10a3a00f1e486810f808ef6c83f2
Adson Rodrigues <adson.develop@gmail.com>
2020-05-17 16:54:27 -0300

    NEW: Adicionando script de criação de ranking em json

---
+++ implementacao/ranking/ranking.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+
+def ler_csv():
+    with open('ranking.csv', 'r') as arquivo_csv:
+        leitor = csv.reader(arquivo_csv, delimiter=',')
+        next(leitor)
+        ranking = []
+        for coluna in leitor:
+            patente = {
+                    "level": coluna[0],
+                    "name": coluna[1],
+                    "xp": coluna[2],
+                    "level_up": coluna[3]
+                  }
+            ranking.append(patente)
+    return ranking
+
+
+def gerar_json(ranking):
+    with open('ranking.json', 'w') as arquivo_json:
+        json.dump(ranking, arquivo_json)
+
+
+def main():
+    ranking = ler_csv()
+    gerar_json(ranking)
+
+
+if __name__ == "__main__":
+    main()
