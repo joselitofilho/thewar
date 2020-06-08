77a4ebe10330a3a5e7edfd197b5743e64bc5c6e0
Joselito Viveiros Nogueira Filho <joselito.nogueira@polibrasnet.com.br>
2020-06-08 17:16:38 -0300

    Memes e sons baixando do google drive.

--- start_game.prod.sh
+++ start_game.prod.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 cd /app # Dentro do container docker deve ser a pasta /app, mas equivale a pasta implementação/server 
+
+pip install gdown
+
+gdown --id 12x46KaDlY5d1OzXJtEERGDoEaI6bPV-O
+mkdir -p webdir/imagens/memes/
+unzip -u memes.zip -d webdir/imagens/
+rm memes.zip
+
+gdown --id 1b9W-7M-QltBvurp4cMXgWbvutJe-K7Uq
+mkdir -p webdir/sons/
+unzip -u sons.zip -d webdir/
+rm sons.zip
+
 FILE="war.db"
 if [ ! -f "$FILE" ]; then
 	echo "Creating database..."
