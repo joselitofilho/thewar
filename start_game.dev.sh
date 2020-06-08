901c73374aa015fe906d56083851831b02b503c8
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2020-06-08 17:16:38 -0300

    Memes e sons baixando do google drive.

--- start_game.dev.sh
+++ start_game.dev.sh
@@ -2,6 +2,18 @@
 
 cd implementacao/server
 
+pip install gdown
+
+gdown --id 12x46KaDlY5d1OzXJtEERGDoEaI6bPV-O
+mkdir -p webdir/imagens/memes/
+unzip -u memes.zip -d webdir/imagens/
+rm memes.zip
+
+gdown --id 1b9W-7M-QltBvurp4cMXgWbvutJe-K7Uq
+mkdir -p webdir/sons/
+unzip -u sons.zip -d webdir/
+rm sons.zip
+
 FILE="war.db"
 if [ ! -f "$FILE" ]; then
 	echo "Creating database..."
