61ae2894494636b4a5c170abc13aa7ecebc758c5
Joselito Viveiros Nogueira Filho <joselito.nogueira@polibrasnet.com.br>
2020-07-31 20:07:10 -0300

    Adicionando imagens dos trofeus e comandos.

--- implementacao/server/update_assets.sh
+++ implementacao/server/update_assets.sh
@@ -4,21 +4,28 @@ if [ ! -f "memes.zip" ]; then
   gdown --id 12x46KaDlY5d1OzXJtEERGDoEaI6bPV-O
   mkdir -p webdir/imagens/memes/
   unzip -u memes.zip -d webdir/imagens/
-  #rm memes.zip  # Please, remove manually.
+  rm memes.zip
 fi
 
 if [ ! -f "sons.zip" ]; then
   gdown --id 1b9W-7M-QltBvurp4cMXgWbvutJe-K7Uq
   mkdir -p webdir/sons/
   unzip -u sons.zip -d webdir/
-  #rm sons.zip  # Please, remove manually.
+  rm sons.zip
 fi
 
 if [ ! -f "videos.zip" ]; then
   gdown --id 1DLO1aRCZFgJZeGQoVnlQZpZLVuPHiZ4F
   mkdir -p webdir/videos/
   unzip -u videos.zip -d webdir/
-  #rm videos.zip  # Please, remove manually.
+  rm videos.zip
+fi
+
+if [ ! -f "trofeus.zip" ]; then
+  gdown --id 14_cIOufhgvm01vlETuJrHB5WynAMFlrq
+  mkdir -p webdir/trofeus/
+  unzip -u trofeus.zip -d webdir/
+  rm trofeus.zip
 fi
 
 gdown --id 1fDOrGiP1gqAUehyAOhZgRMwHyeVgiu81
